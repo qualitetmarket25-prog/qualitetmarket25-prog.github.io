dae1321b248434472c57f75cc41b0c1b5a04514e
qualitetmarket25-prog <qualitetmarket25@gmail.com>
2026-03-01 15:59:57 +0100

    Delete store/store.js

--- store/store.js
+++
@@ -1,9 +0,0 @@
-function addToCart() {
-  const product = {
-    name: "Lunera Pro Lamp™",
-    price: 79.99
-  };
-
-  localStorage.setItem("cart", JSON.stringify(product));
-  window.location.href = "cart.html";
-}
